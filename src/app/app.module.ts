a2e1563bf111a1d9355addbb33ae297b656c5a0d
Jhonata Camargo <jhonata@collegare.com.br>
2017-07-19 19:01:16 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { IntroPage } from "../pages/intro/intro";
 
 @NgModule({
   declarations: [
     MyApp,
+    IntroPage,
     HomePage,
     ListPage
   ],
@@ -22,6 +24,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    IntroPage,
     HomePage,
     ListPage
   ],
